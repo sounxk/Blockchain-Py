d745a5d5faf319df7e9599badc092ba2610a5e63
Sounak Ghosh <sounakghosh.official@gmail.com>
2020-06-03 12:38:19 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -15,3 +15,30 @@ class BlockChain(object):
         self.nodes = set()
         # create the genesis block
         self.new_block(previous_hash=1, proof=100)
+        
+    @staticmethod
+    def hash(block):
+        # hashes a block
+        # also make sure that the transactions are ordered otherwise we will have insonsistent hashes!
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def new_block(self, proof, previous_hash=None):
+        # creates a new block in the blockchain
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # reset the current list of transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    @property
+    def last_block(self):
+        # returns last block in the chain
+        return self.chain[-1]
