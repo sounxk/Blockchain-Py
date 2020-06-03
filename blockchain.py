b8637cf53dcf00b63bc456b22a703eda4bf648e2
Sounak Ghosh <sounakghosh.official@gmail.com>
2020-06-03 12:38:46 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -15,7 +15,7 @@ class BlockChain(object):
         self.nodes = set()
         # create the genesis block
         self.new_block(previous_hash=1, proof=100)
-        
+
     @staticmethod
     def hash(block):
         # hashes a block
@@ -42,3 +42,126 @@ class BlockChain(object):
     def last_block(self):
         # returns last block in the chain
         return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
+        # adds a new transaction into the list of transactions
+        # these transactions go into the next mined block
+        self.current_transactions.append({
+            "sender":sender,
+            "recient":recipient,
+            "data":amount,
+        })
+        return int(self.last_block['index'])+1
+
+    def proof_of_work(self, last_proof):
+        # simple proof of work algorithm
+        # find a number p' such as hash(pp') containing leading 4 zeros where p is the previous p'
+        # p is the previous proof and p' is the new proof
+        proof = 0
+        while self.validate_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def validate_proof(last_proof, proof):
+        # validates the proof: does hash(last_proof, proof) contain 4 leading zeroes?
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        # add a new node to the list of nodes
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def full_chain(self):
+        # xxx returns the full chain and a number of blocks
+        pass
+
+
+# initiate the node
+app = Flask(__name__)
+# generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+# initiate the Blockchain
+blockchain = BlockChain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+
+    # first we need to run the proof of work algorithm to calculate the new proof..
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # we must recieve reward for finding the proof in form of receiving 1 Coin
+    blockchain.new_transaction(
+        sender=0,
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # forge the new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "Forged new block.",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response, 200)
+
+@app.route('/transaction/new', methods=['GET'])
+def new_transaction():
+
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amont']
+
+    if not all(k in values for k in required):
+        return 'Missing values.', 400
+
+    # create a new transaction
+    index = blockchain.new_transaction(
+        sender = values['sender'],
+        recipient = values['recipient'],
+        amount = values['amount']
+    )
+
+    response = {
+        'message': f'Transaction will be added to the Block {index}',
+    }
+    return jsonify(response, 200)
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    print('values',values)
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    # register each newly added node
+    for node in nodes: blockchain.register_node(node)
+
+    response = {
+        'message': "New nodes have been added",
+        'all_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
