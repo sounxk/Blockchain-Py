29a8fec2c42956b0b1b8ce7ae0ca4e4b5622bfc5
Sounak Ghosh <sounakghosh.official@gmail.com>
2020-06-03 21:32:03 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -147,3 +147,92 @@ class BlockChain(object):
             block_data['transactions'],
             timestamp=block_data['timestamp']
         )
+
+    # initiation of the node
+app = Flask(__name__)
+
+# generation of globally unique address 
+node_identifier = str(uuid4()).replace('-', '')
+
+# initiate the Blockchain
+blockchain = BlockChain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+
+    # first the PoW algorithm will run to find the new proof
+    last_block = blockchain.last_block
+    last_proof = get_last_block['proof']
+    proof = blockchain.create_proof_of_work(last_proof)
+
+    # 1 coin is added as a reqard for mining
+    blockchain.create_new_transaction(
+        sender=0,
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # forge the new block by adding it to the chain
+    previous_hash = blockchain.get_block_hash(last_block)
+    block = blockchain.create_new_block(proof, previous_hash)
+
+    response = {
+        'message': "Forged new block.",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response, 200)
+
+@app.route('/transaction/new', methods=['GET'])
+def create_new_transaction():
+
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amont']
+
+    if not all(k in values for k in required):
+        return 'Missing values.', 400
+
+    # create a new transaction
+    index = blockchain.create_new_transaction(
+        sender = values['sender'],
+        recipient = values['recipient'],
+        amount = values['amount']
+    )
+
+    response = {
+        'message': f'Transaction will be added to the Block {index}',
+    }
+    return jsonify(response, 200)
+
+@app.route('/chain', methods=['GET'])
+def is_valid_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def create_node():
+    values = request.get_json()
+
+    print('values',values)
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    # register each newly added node
+    for node in nodes: blockchain.register_node(node)
+
+    response = {
+        'message': "New nodes have been added",
+        'all_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
